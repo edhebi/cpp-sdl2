7d2d60eddc1a650c65001d8fa41228ac54a0d22a
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-03-26 13:57:22 +0100

    add wrapper for the rest of the game controller API

--- example/main.cpp
+++ example/main.cpp
@@ -6,26 +6,39 @@
 
 #include <iostream>
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
 	(void)argc;
 	(void)argv;
 
 	std::srand(unsigned(std::time(nullptr)));
 
-	// The following classes manages the lifetime of SDL declared resources RAII style
+	// The following classes manages the lifetime of SDL declared resources RAII
+	// style
 
-	auto root = sdl::Root(SDL_INIT_EVENTS);
+	auto root = sdl::Root(SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER);
 
-	auto window = sdl::Window{ "Random Colors", {600, 600} };
+	auto window = sdl::Window{"Random Colors", {600, 600}};
 
 	auto renderer = window.make_renderer();
-	
+
 	auto color = sdl::Color::Black();
 
-	auto done   = false;
+	auto done	= false;
 	auto redraw = true;
-	auto event = sdl::Event{};
+	auto event	= sdl::Event{};
+
+	// This utility function will open all the game controllers connected to the
+	// system that are known from the SDL GameController API
+	auto controllers = sdl::GameController::open_all_available_controllers();
+	if (!controllers.empty())
+	{
+		std::cout << "Detected controllers:\n";
+		for (auto& controller : controllers)
+		{
+			std::cout << "  " << controller.get_name() << '\n';
+		}
+	}
 
 	while (!done)
 	{
@@ -35,24 +48,23 @@ int main(int argc, char * argv[])
 			renderer.present();
 			redraw = false;
 		}
-	
-		event.wait();
-		
-		if (event.type == SDL_QUIT)
-		{
-			done = true;
-		}
 
-		if (event.type == SDL_MOUSEBUTTONUP)
+		event.wait();
+
+		switch (event.type)
 		{
+		case SDL_QUIT: done = true; break;
+		case SDL_MOUSEBUTTONUP:
+		case SDL_KEYUP:
+		case SDL_CONTROLLERBUTTONDOWN:
 			color.r = std::rand() % 256;
 			color.g = std::rand() % 256;
 			color.b = std::rand() % 256;
-			redraw = true;
+			redraw	= true;
+		default: break;
 		}
 	}
 	return 0;
 
 	// Cleanup is done automatically
-
 }
--- sources/cpp-sdl2/game_controller.hpp
+++ sources/cpp-sdl2/game_controller.hpp
@@ -1,31 +1,30 @@
 #pragma once
 
 #include <SDL_gamecontroller.h>
+#include <chrono>
+#include <string>
 
 namespace sdl
 {
-
 ///\brief Represent a gamepad (game controller)
 class GameController
 {
 public:
-	GameController(int joystick_index) :
-		controller_(SDL_GameControllerOpen(joystick_index))
+	GameController(int joystick_index)
+		: controller_(SDL_GameControllerOpen(joystick_index))
 	{
-		if(!controller_)
+		if (!controller_)
 		{
-			//error
+			// error
 		}
 	}
 
-	GameController() = default;
-
+	GameController()					  = default;
 	GameController(GameController const&) = delete;
 	GameController& operator=(GameController const&) = delete;
-	GameController(GameController&& other)
-	{
-		move_from(other);
-	}
+
+	GameController(GameController&& other) { move_from(other); }
+
 	GameController& operator=(GameController&& other)
 	{
 		move_from(other);
@@ -34,26 +33,131 @@ public:
 
 	~GameController()
 	{
-		if (controller_)
-			SDL_GameControllerClose(controller_);
+		if (controller_) SDL_GameControllerClose(controller_);
 	}
 
-	SDL_GameController* ptr() const
+	SDL_GameController* ptr() const { return controller_; }
+
+	bool is_attached() const
 	{
-		return controller_;
+		return SDL_GameControllerGetAttached(controller_) == SDL_TRUE;
 	}
 
+	int16_t get_axis(SDL_GameControllerAxis axis) const
+	{
+		return SDL_GameControllerGetAxis(controller_, axis);
+	}
+
+	int8_t get_button(SDL_GameControllerButton button) const
+	{
+		return SDL_GameControllerGetButton(controller_, button);
+	}
+
+	int rumble(
+		uint16_t				  low_freq,
+		uint16_t				  high_freq,
+		std::chrono::milliseconds duration)
+	{
+		return rumble(
+			low_freq, high_freq, static_cast<uint32_t>(duration.count()));
+	}
+
+	int rumble(
+		uint16_t low_freq, uint16_t high_freq, uint32_t millisec_duration)
+	{
+		return SDL_GameControllerRumble(
+			controller_, low_freq, high_freq, millisec_duration);
+	}
+
+	std::string get_name()
+	{
+		if (!controller_) return {};
+
+		return {SDL_GameControllerName(controller_)};
+	}
+
+	static std::string get_controller_name(int joystick_index)
+	{
+		// todo error when SDL return null
+		return {SDL_GameControllerNameForIndex(joystick_index)};
+	}
+
+	// static std::string get_axis_name(SDL_GameControllerAxis axis)
+	//{
+	//	//todo error when SDL return null
+	//	return { SDL_GameControllerGetStringForAxis(axis) };
+	//}
+
+	// static std::string get_button_name(SDL_GameControllerButton button)
+	//{
+	//	//todo error when SDL return null
+	//	return { SDL_GameControllerGetStringForButton(button) };
+	//}
+
+	// Bindings management wrappers
+	static int load_mapping_database(std::string const& file_path)
+	{
+		return load_mapping_database(file_path.c_str());
+	}
+
+	static int load_mapping_database(const char* file_path)
+	{
+		const auto state = SDL_GameControllerAddMappingsFromFile(file_path);
+		if (state < 0)
+		{
+			// error
+		}
+
+		return state;
+	}
+
+	static int add_mapping(std::string const& mapping_string)
+	{
+		return add_mapping(mapping_string.c_str());
+	}
+	static int add_mapping(const char* mapping_string)
+	{
+		const auto state = SDL_GameControllerAddMapping(mapping_string);
+
+		if (state < 0)
+		{
+			// error
+		}
+
+		return state;
+	}
+
+	// convinience functions
+	static std::vector<GameController> open_all_available_controllers()
+	{
+		std::vector<GameController> controllers;
+
+		for (int nb_sticks = SDL_NumJoysticks(), i = 0; i < nb_sticks; ++i)
+		{
+			if (SDL_IsGameController(i))
+			{
+				try
+				{
+					controllers.emplace_back(i);
+				}
+				catch (sdl::Exception const& e)
+				{
+					// could not open this controller
+					continue;
+				}
+			}
+		}
+
+		return controllers;
+	}
 
 private:
-
 	void move_from(GameController& other)
 	{
-		controller_ = other.ptr();
+		controller_		  = other.ptr();
 		other.controller_ = nullptr;
 	}
 
 	SDL_GameController* controller_ = nullptr;
-
 };
-
-}
+} // namespace sdl
