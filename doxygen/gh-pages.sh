708323bd10a70e1b1b5d431a691b811edd6674d7
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-06-22 11:49:06 +0200

    comment what's going on in gh-pages.sh

--- doxygen/gh-pages.sh
+++ doxygen/gh-pages.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-echo "will setup gh-pages soon!"
-
-
+#get another copy of this project
 git clone https://github.com/Edhebi/cpp-sdl2 cpp-sdl2-gh-pages
 cd cpp-sdl2-gh-pages
+
+#Access the gh-pages branch
+git checkout gh-pages
+
+#configure a valid commiter 
 git config user.name "Ybalrid via Travis"
 git config user.email "ybalrid+travis@ybalrid.info"
-git remote origin set-url https://Ybalrid:$github_doxygen_access_token@github.com/Edhebi/cpp-sdl2
-git checkout gh-pages
-git checkout af860f6dad3a6b2b65d9e8e690d53795cae49e7f
+
+#Ybalrid has setup a secured access token
+git remote set-url --push origin https://Ybalrid:$github_doxygen_access_token@github.com/Edhebi/cpp-sdl2
+
+#D E S T R O Y the last commit, make it if did not exist like, at all.
+git reset --hard HEAD~1 #remove last commit 
+
+#E R A S E  the existing Doxygen site
 rm -rf doc/*
+
+#Commit the new doxigen pages
 cp -rv ../output/html/* doc/
 git add doc
 git commit -m "Travis automatic documentation update"
-git push --force origin gh-pages
 
+#F O R C E   P U S H  to the gh-pages branch on GitHub using Ybalrid's creds
+git push --force origin gh-pages #BE CAREFUL WITH THIS
 
